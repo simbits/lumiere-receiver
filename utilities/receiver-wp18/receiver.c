63593755faccab333e3bcfeb08f35a991793a799
Simon de Bakker <simon@simbits.nl>
2016-10-19 13:25:23 +0200

    simple signal buffer

--- utilities/receiver-wp18/receiver.c
+++ utilities/receiver-wp18/receiver.c
@@ -21,14 +21,101 @@
 
 #define MAX_SEQ_LINES   1024
 
-//#define PIN 21    /* for B+ PI */
-#define PIN 30      /* for Projector */
-#define PINOUT 31
+#define INPIN 30      /* for Projector */
+#define OUTPIN 31
 #define LED_PIN 19      /* LED pin */
 
-#define READVAL()  bcm2835_gpio_lev( PIN )
+#define READVAL()  bcm2835_gpio_lev( INPIN )
 
 #include <stdio.h>
+
+char *sequence_lines[MAX_SEQ_LINES];
+
+int open_sequence_file(const char *path)
+{
+    FILE* file = NULL;
+    int i = 0;
+    char line[512];
+
+    if ((file = fopen(path, "r")) == NULL) {
+        printf("Error reading sequence: %s\n", strerror(errno));
+        return 0;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        printf("%d: %s", i+1, line);
+        sequence_lines[i++] = strdup(line);
+        if (i >= MAX_SEQ_LINES) {
+            printf("Warning: too many sequence lines, truncating sequence\n");
+            break;
+        }
+    }
+
+    fclose(file);
+
+    return i;
+}
+
+static inline uint64_t wait_for_rising()
+{
+    uint64_t start = bcm2835_st_read();
+    while ( ! READVAL() )
+        bcm2835_delayMicroseconds(200);
+    bmc2835_gpio_write(OUTPIN, HIGH);
+    return bcm2835_st_read() - start;
+}
+
+static inline uint64_t wait_for_falling()
+{
+    uint64_t start = bcm2835_st_read();
+    while ( READVAL() )
+        bcm2835_delayMicroseconds(200);
+    bmc2835_gpio_write(OUTPIN, LOW);
+    return bcm2835_st_read() - start;
+}
+
+static inline uint64_t wait_for_high_pulse()
+{
+    wait_for_rising();
+    return wait_for_falling();
+}
+
+static inline uint64_t wait_for_low_pulse()
+{
+    wait_for_falling();
+    return wait_for_rising();
+}
+
+void execute_line(unsigned int n)
+{
+    char cmd;
+    char *line;
+
+    if (n >= MAX_SEQ_LINES) {
+        printf("Error: sequence line %d out of bounds\n", n);
+        return;
+    }
+
+    line = sequence_lines[n];
+    cmd = *line++;
+    line++;
+
+    switch (cmd) {
+        case PLAY:
+            printf("playing line %d: %s\n", n, line);
+            system(line);
+            break;
+        case DELAY:
+            {
+                unsigned int delay = (unsigned int)atoi(line);
+                printf("delaying for %d us\n", delay);
+                bcm2835_delay(delay);
+                break;
+            }
+        default:
+            break;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -40,23 +127,52 @@ int main(int argc, char **argv)
     if (!bcm2835_init())
         return 1;
 
-    bcm2835_gpio_fsel(PINOUT, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_write(PINOUT, HIGH);
-    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
-    bcm2835_gpio_fen(PIN);
+    bcm2835_gpio_fsel(OUTPIN, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(INPIN, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(INPIN, BCM2835_GPIO_PUD_OFF);
+    bcm2825_gpio_write(OUTPIN, bcm2835_gpio_lev(INPUT));
 
     srand(time(NULL));
 
-    while (1) {
-        if (bcm2835_gpio_eds(PIN)) {
-            bcm2835_gpio_write(PINOUT, LOW);
-            bcm2835_delayMicroseconds(2000);
-            bcm2835_gpio_write(PINOUT, HIGH);
-            bcm2835_gpio_set_eds(PIN);
-        } else {
-            bcm2835_delayMicroseconds(100);
+    if (argc < 2)
+        path = "/opt/lumiere/bin/sequences/lumiere_default_sequence.txt";
+    else {
+        path = argv[1];
+        if (argc >= 3) {
+            int level = 0;
+
+            if ( READVAL() ) level++;
+            bcm2825_gpio_write(OUTPIN, bcm2835_gpio_lev(INPUT));
+            bcm2835_delay(200);
+            if ( READVAL() ) level++;
+            bcm2825_gpio_write(OUTPIN, bcm2835_gpio_lev(INPUT));
+            bcm2835_delay(200);
+            if ( READVAL() ) level++;
+            bcm2825_gpio_write(OUTPIN, bcm2835_gpio_lev(INPUT));
+
+            if (level >= 2) {
+                path = argv[2];
+            } else {
+                path = argv[1];
+            }
         }
+    }
+    sequences = open_sequence_file(path);
+
+    if (sequences <= 0) {
+        printf("Warning no sequences read\n");
+        exit(1);
+    }
+
+    while (1) {
+        execute_line(pulse_counter);
+
+        if (++pulse_counter >= sequences)
+            pulse_counter = 0;
+
+        bcm2835_delayMicroseconds(100);
+
+        wait_for_high_pulse();
     }
 
     bcm2835_close();
